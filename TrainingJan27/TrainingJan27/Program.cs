a80da0fa559a7c30c3ef84e9e0a5f7de19e5ceb5
Hemang Maan <hemangmaan@gmail.com>
2022-01-28 09:00:09 +0530

    training

---
+++ TrainingJan27/TrainingJan27/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TrainingJan27
+{
+    public class Intrest
+    {
+        public double SimpleIntrest(double principal,double rate,double time)
+        {
+            return principal * (1 + rate * time);
+        }
+        public double discount(double amount,double discount)
+        {
+            return amount * discount/100;
+        }
+    }
+}
---
+++ TrainingJan27/TrainingJan27Tests/IntrestTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrainingJan27;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainingJan27.Tests
+{
+    [TestClass()]
+    public class IntrestTests
+    {
+        [TestMethod()]
+        public void SimpleIntrestTest()
+        {
+            Intrest i = new Intrest();
+            double expected = 11937.50;
+            double actual = i.SimpleIntrest(10000, 0.03875, 5);
+            Assert.AreEqual(expected, actual);
+            
+        }
+        [TestMethod()]
+        public void discount()
+        {
+            Intrest i = new Intrest();
+            double expected = 3000;
+            double actual = i.discount(10000, 30);
+            Assert.AreEqual(expected, actual);
+            
+        }
+    }
+}
